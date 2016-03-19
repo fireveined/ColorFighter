9b2bfa39b44a3fa936f5156aa3ee9a2de3a83cfe
fireveined <adde197@gmail.com>
2016-03-19 13:44:31 +0100

    heroku integration

--- server.js
+++ server.js
@@ -16,17 +16,15 @@ server.create();
 server.run();
 
 var  ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var  port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var  port = process.env.PORT || 8080;
 
 if (typeof ipaddress === "undefined") {
 	//  Log errors on OpenShift but continue w/ 127.0.0.1 - this
 	//  allows us to run/test the app locally.
-	console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-	ipaddress = "192.168.1.100";
+	console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1. PORT: '+port);
+	ipaddress = "127.0.0.1";
 };
 
-http.listen(port, ipaddress, function () {
-	console.log("Server is listening on port " + port);
-});
+http.listen(port);
 
 
